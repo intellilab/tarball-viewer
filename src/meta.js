c034fe14ee72f894dd60d4110c9e9f70f3d6c587
Gerald <gera2ld@live.com>
2021-03-24 23:25:27 +0800

    chore: use rollup-plugin-userscript

--- src/meta.js
+++ src/meta.js
@@ -3,10 +3,13 @@
 // @namespace   https://gera2ld.space/
 // @description View content of tarballs without download them.
 // @match       *://www.npmjs.com/package/*
-// @grant       GM_xmlhttpRequest
-// @grant       GM_registerMenuCommand
 // @version     process.env.VERSION
 // @author      process.env.AUTHOR
 // @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@1,npm/@violentmonkey/ui@0.5
 // @require     https://cdn.jsdelivr.net/combine/npm/pako@2.0.3/dist/pako.min.js,npm/@gera2ld/tarjs@^0.1.2
 // ==/UserScript==
+
+/**
+ * The @grant's used in your source code will be added automatically by rollup-plugin-userscript.
+ * However you have to add explicitly those used in required resources.
+ */
