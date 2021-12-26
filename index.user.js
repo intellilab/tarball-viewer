7043d8c342d8b8523bec9d490a4643291a91b1fe
gera2ld <gera2ld@live.com>
2021-12-26 05:23:13 +0000

    Deploying to release from @ intellilab/tarball-viewer.user.js@ad3da83d66e3bb5c15e7cb6aa3f55dd7834a2d28 🚀

--- index.user.js
+++ index.user.js
@@ -181,7 +181,7 @@ let reader;
 let items;
 let panel;
 let active;
-const matches = window.location.pathname.match(/^\/package\/(.*?)(?:\/v\/([\d.]+))?$/);
+const matches = window.location.pathname.match(/^\/package\/(.*?)(?:\/v\/([a-z0-9.-]+))?$/);
 
 if (matches) {
   panel = VM.getPanel({
